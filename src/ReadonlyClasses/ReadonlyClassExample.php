a1eeca1c91e6a9ede286bc54f9035b63620b7e08
Wu Mobius <energy908@gmail.com>
2023-01-07 17:07:28 +0800

    Readonly classes: 宣告 field 的限制

--- src/ReadonlyClasses/ReadonlyClassExample.php
+++ src/ReadonlyClasses/ReadonlyClassExample.php
@@ -6,6 +6,17 @@ readonly class ReadonlyClassExample
 {
     public int $bar;
 
+    /**
+     * 宣告的 field 必須要有型別宣告
+     */
+    // public $notAllow1;
+    // Fatal error: Readonly property Foo::$bar must have type
+
+    /**
+     * 宣告的 field 不可以是 static
+     */
+    // public static int $notAllow2;
+    // Fatal error: Readonly class Foo cannot declare static properties
     public function __construct()
     {
         $this->bar = 1;
